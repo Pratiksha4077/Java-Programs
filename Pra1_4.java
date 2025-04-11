a483748229e4610649e180e44c52d8164fba72ac
Pratiksha4077 <pratikshapatil4077@gmail.com>
2025-04-11 10:19:47 +0530

    Update Pra1_4.java

--- Pra1_4.java
+++ Pra1_4.java
@@ -1,126 +1,28 @@
-import java.util.*;
-class Pra1_4
-{
-	public static void main(String args[])
-	{
-		Scanner sc=new Scanner(System.in);
+import java.util.Arrays;
 
-		int m1[][]=new int[2][2];
-		int m2[][]=new int[2][2];
-		int res[][]=new int[2][2];
-		int ch,n;
+public class Pra1_5 {
+    public static void mergeArrays(int[] arr1, int size1, int[] arr2, int size2, int[] merged) {
+        int i = 0, j = 0, k = 0;
+        
+        while (i < size1) {
+            merged[k++] = arr1[i++];
+        }
+        while (j < size2) {
+            merged[k++] = arr2[j++];
+        }
 
-		System.out.print("Enter the element for matrix1 ");
-		for(int i=0;i<2;i++)
-		{
-			for(int j=0;j<2;j++)
-			{
-				m1[i][j]=sc.nextInt();
-			}
-		}
+	Arrays.sort(merged);
+    }
 
-		System.out.print("\nEnter the element for matrix2 ");
-		for(int i=0;i<2;i++)
-		{
-			for(int j=0;j<2;j++)
-			{
-				m2[i][j]=sc.nextInt();
-			}
-		}
-
-		System.out.println("\nMatrix 1:");
-		for(int i=0;i<2;i++)
-		{
-			for(int j=0;j<2;j++)
-			{
-				System.out.print(m1[i][j]+" ");
-			}
-			System.out.println();
-		}
-
-		System.out.println("\nMatrix 2:");
-		for(int i=0;i<2;i++)
-		{
-			for(int j=0;j<2;j++)
-			{
-				System.out.print(m2[i][j]+" ");
-			}
-			System.out.println();
-		}
-	
-
-		System.out.println("\n1.Addition\n2.Multiplication \n3.Transpose");
-
-		do
-		{
-		System.out.print("Enter choice:");
-		ch=sc.nextInt();
-
-		switch(ch)
-		{
-
-			case 1:
-				for(int i=0;i<2;i++)
-				{
-					for(int j=0;j<2;j++)
-					{
-						res[i][j]=m1[i][j]+m2[i][j];
-						
-					}
-				}
-				System.out.println("Addition of matrix:");
-				for(int i=0;i<2;i++)
-				{
-					for(int j=0;j<2;j++)
-					{
-						System.out.print(res[i][j]+" ");
-					}
-					System.out.println();
-				}
-				break;
-
-			case 2:
-				for(int i=0;i<2;i++)
-				{
-					for(int j=0;j<2;j++)
-					{
-						res[i][j]=m1[i][j]*m2[i][j];
-						
-					}
-				}
-				System.out.println("Multiplication of matrix:");
-				for(int i=0;i<2;i++)
-				{
-					for(int j=0;j<2;j++)
-					{
-						System.out.print(res[i][j]+" ");
-					}
-					System.out.println();
-
-				}
-				break;
-
-			case 3:
-				System.out.println("Transpose of matrix 1");
-				for(int i=0;i<2;i++)
-				{
-					for(int j=0;j<2;j++)
-					{
-						System.out.print(m1[j][i]+" ");
-					}
-					System.out.println();
-				}
-				break;
-
-			default:
-				System.out.println("Invalid Choice");
-
-		}
-
-		System.out.println("Click '1' for next operation and '0' for exit");
-		n=sc.nextInt();
-	}while(n==1);
-
-	}
+    public static void main(String[] args) {
+        int[] arr1 = {9, 3, 6, 1};
+        int[] arr2 = {8, 3, 2, 7};
+        int size1 = arr1.length;
+        int size2 = arr2.length;
+        int[] merged = new int[size1 + size2];
+        
+        mergeArrays(arr1, size1, arr2, size2, merged);
+        
+        System.out.println("Merged array: " + Arrays.toString(merged));
+    }
 }
-
